0ae39ee4ff00e904b3b4afdbc2c252e49d130c40
Casey Flynn <cflynn.us@gmail.com>
2020-04-24 20:56:30 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@
 git diff
 git config --global user.email "readme-bot@example.com"
 git config --global user.name "README-bot"
+git add ./README.md
 git diff --quiet || (git add README.md && git commit -m "Updated README")
 git push
