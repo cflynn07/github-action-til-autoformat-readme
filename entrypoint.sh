f4b66a5baba2210c9d70f16064f9b14f323cd482
Casey Flynn <cflynn.us@gmail.com>
2023-09-13 16:21:04 +0700

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-/root/main;
-git diff
+git config --global --add safe.directory /github/workspace
 git config --global user.email "readme-bot@example.com"
 git config --global user.name "README-bot"
+
+/root/main;
+
+git diff
 echo "git add README.md && git commit -m \"Updated README\""
 git add README.md && git commit -m "Updated README"
 echo "git push"
